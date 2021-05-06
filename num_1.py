5394d340c385f223be6ea25a6825a6c0bf0ecf98
ais123-art <anisv123@gmail.com>
2021-05-06 10:28:48 +0300

    Initial pic_num

---
+++ num_1.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+number = np.random.randint(1, 101)
+
+def game_core_v2(number):
+    '''Последовательно увеличиваем predict на значение 4. Если
+    значение predict становится > загаданного number, то уменьшаем
+    predict на 5, пока predict не станет равен number. Функция
+    принимает загаданное число и шаг, а возвращает число попыток
+    '''
+    count = 1
+    predict = np.random.randint(1, 101)
+    while number != predict:
+        count += 1
+        if number > predict: 
+            predict += 4
+        elif number < predict: 
+            predict -= 5
+    return (count) # выход из цикла, если угадали
+
+
+def score_game(game_core_v2):
+        '''Запускаем игру 1000 раз, чтобы узнать, как быстро игра угадывает число'''
+        count_ls = []
+        np.random.seed(1)  # фиксируем RANDOM SEED, чтобы ваш эксперимент был воспроизводим!
+        random_array = np.random.randint(1, 101, size=(1000))
+        for number in random_array:
+            count_ls.append(game_core_v2(number))
+        score = int(np.mean(count_ls))
+        print(f"Ваш алгоритм угадывает число в среднем за {score} попыток")
+        return (score)
+
+
+# Проверяем
+score_game(game_core_v2)
+
+count = game_core_v2(number)
+
+print(f"Вы угадали число {number} за {count} попыток.")
